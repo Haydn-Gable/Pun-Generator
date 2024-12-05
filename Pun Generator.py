df596efe71e281562c76048862a1c61dd37efc7f
ZenoMessiah <149208555+ZenoMessiah@users.noreply.github.com>
2024-12-05 00:34:05 -0500

    Update Pun Generator.py

--- Pun Generator.py
+++ Pun Generator.py
@@ -1,7 +1,8 @@
 import random
 
 punList = ['Did you hear about the guy who had his whole left side cut off? He is all right now.',
-           'Road work ahead? I sure hope it does!']
+           'Road work ahead? I sure hope it does!',
+          'What do you call an old snowman? Water.']
 
 ranNum = random.randint(0, len(punList) - 1)
 
