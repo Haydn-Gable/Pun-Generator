21c27c42b2212e864bf8f6e1043d14c9da4a17e2
Haydn-Gable <149208555+Haydn-Gable@users.noreply.github.com>
2024-12-05 00:07:38 -0500

    Add files via upload

---
+++ Pun Generator.py
@@ -0,0 +1,8 @@
+import random
+
+punList = ['Did you hear about the guy who had his whole left side cut off? He is all right now.',
+           'Road work ahead? I sure hope it does!']
+
+ranNum = random.randint(0, len(punList) - 1)
+
+print(punList[ranNum])
