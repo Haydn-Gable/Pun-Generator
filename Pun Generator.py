4d7c89117f319b70c5a591d85b458bbcc060da7e
ZenoMessiah <149208555+ZenoMessiah@users.noreply.github.com>
2024-12-12 13:11:56 -0500

    Return to the status quo

--- Pun Generator.py
+++ Pun Generator.py
@@ -11,23 +11,7 @@ punList = ['Did you hear about the guy who had his whole left side cut off? He i
            'How did the skeleton know it was going to rain? He felt it in his bones.',
            'What do you call an exploding duck? A firequacker.']
 
-print("How many puns should be generated? Pick a number between 1 and ", len(punList))
-punAmount = int(input())
-while punAmount <= 1 or punAmount >= len(punList):
-    print("Out of range. Pick a number in range")
-    punAmount = int(input())
-
-ranList = []
-
-def printPun():
-    ranNum = random.randint(0, len(punList) - 1)
-    ranList.append(ranNum)
-    for i in range (len(ranList)):
-            while ranNum == ranList[i]:
-                    ranNum = random.randint(0, len(punList) - 1)
-
-for i in range (punAmount):
-    printPun()
-    
-for i in range (len(ranList)):
-    print(punList[i])
+def generatePun():
+           ranNum = random.randint(0, len(punList) - 1)
+           print(punList[ranNum])
+generatePun()
